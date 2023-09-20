d5fc73f8c1c8b3af9df724a31874acbf47e52a28
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-09-20 14:46:15 -0400

    Add functionality with flask and json

--- server/app.py
+++ server/app.py
@@ -14,13 +14,59 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    messages_serialized = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+    response = make_response(
+        jsonify(messages_serialized),
+        200
+    )
+    return response
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    if 'body' in data and 'username' in data:
+        new_message = Message(body=data['body'], username=data['username'])
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_message.to_dict()),
+            201
+        )
+        return response
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.filter_by(id=id).first()
+    
+    if 'body' in data:
+        message.body = data['body']
+        db.session.commit()
+
+    response = make_response(
+        jsonify(message.to_dict()),
+        200
+    )
+    return response
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first()
+    db.session.delete(message)
+    db.session.commit()
+
+    response = make_response(
+        jsonify({"message": "Message successfully deleted"}),
+        200
+    )
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
